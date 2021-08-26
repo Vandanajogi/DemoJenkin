b1892615eeb3fe44034bb285ba51dd25603e0632
Vandanajogi <vandanajogi1994@gmail.com>
2021-08-26 16:19:52 +0530

    Adding annotation

--- src/test/java/com/demoexample/SpringControllerTest.java
+++ src/test/java/com/demoexample/SpringControllerTest.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-
+import org.mockito.runners.MockitoJUnitRunner;
 
 import junit.framework.Assert;
 
 
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class SpringControllerTest {
 
 	SpringController con=new SpringController();
